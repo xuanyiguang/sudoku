8f59165cfc9297fbe53685f8b46f28766f2f645d
Ethan Xuan <xuan.yiguang@gmail.com>
2014-10-23 16:02:10 -0700

    ADD function verify_sudoku_solution to avoid redundancy

--- tests/test_sudoku_solutions.py
+++ tests/test_sudoku_solutions.py
@@ -7,6 +7,12 @@ from src.sudoku import *
 Sudoku puzzles and solutions are given or come from online sources
 """
 
+def verify_sudoku_solution(sudoku_input_filename, sudoku_output_filename):
+    sudoku_input = np.loadtxt(sudoku_input_filename,delimiter=",",dtype="i4")
+    sudoku_output = solve_sudoku(sudoku_input)
+    sudoku_expected_output = np.loadtxt(sudoku_output_filename,delimiter=",",dtype="i4")
+    assert (sudoku_expected_output == sudoku_output).all()
+
 def test_solve_sudoku_given_example():
     """ Test if the sudoku solver gives the same solution
     
@@ -14,11 +20,8 @@ def test_solve_sudoku_given_example():
     """
     sudoku_input_filename = "../data/sudoku_example_in.csv"
     sudoku_output_filename = "../data/sudoku_example_out.csv"
-    sudoku_input = np.loadtxt(sudoku_input_filename,delimiter=",",dtype="i4")
-    sudoku_output = solve_sudoku(sudoku_input)
-    sudoku_expected_output = np.loadtxt(sudoku_output_filename,delimiter=",",dtype="i4")
-    assert (sudoku_expected_output == sudoku_output).all()
-    
+    verify_sudoku_solution(sudoku_input_filename, sudoku_output_filename)
+
 def test_solve_sudoku_easy1():
     """ Test if the sudoku solver gives the same solution
     
@@ -27,10 +30,7 @@ def test_solve_sudoku_easy1():
     """
     sudoku_input_filename = "../data/sudoku_easy1_in.csv"
     sudoku_output_filename = "../data/sudoku_easy1_out.csv"
-    sudoku_input = np.loadtxt(sudoku_input_filename,delimiter=",",dtype="i4")
-    sudoku_output = solve_sudoku(sudoku_input)
-    sudoku_expected_output = np.loadtxt(sudoku_output_filename,delimiter=",",dtype="i4")
-    assert (sudoku_expected_output == sudoku_output).all()
+    verify_sudoku_solution(sudoku_input_filename, sudoku_output_filename)
 
 def test_solve_sudoku_easy2():
     """ Test if the sudoku solver gives the same solution
@@ -40,10 +40,7 @@ def test_solve_sudoku_easy2():
     """
     sudoku_input_filename = "../data/sudoku_easy2_in.csv"
     sudoku_output_filename = "../data/sudoku_easy2_out.csv"
-    sudoku_input = np.loadtxt(sudoku_input_filename,delimiter=",",dtype="i4")
-    sudoku_output = solve_sudoku(sudoku_input)
-    sudoku_expected_output = np.loadtxt(sudoku_output_filename,delimiter=",",dtype="i4")
-    assert (sudoku_expected_output == sudoku_output).all()
+    verify_sudoku_solution(sudoku_input_filename, sudoku_output_filename)
 
 def test_solve_sudoku_easy3():
     """ Test if the sudoku solver gives the same solution
@@ -53,10 +50,7 @@ def test_solve_sudoku_easy3():
     """
     sudoku_input_filename = "../data/sudoku_easy3_in.csv"
     sudoku_output_filename = "../data/sudoku_easy3_out.csv"
-    sudoku_input = np.loadtxt(sudoku_input_filename,delimiter=",",dtype="i4")
-    sudoku_output = solve_sudoku(sudoku_input)
-    sudoku_expected_output = np.loadtxt(sudoku_output_filename,delimiter=",",dtype="i4")
-    assert (sudoku_expected_output == sudoku_output).all()
+    verify_sudoku_solution(sudoku_input_filename, sudoku_output_filename)
 
 def test_solve_sudoku_easy4():
     """ Test if the sudoku solver gives the same solution
@@ -66,10 +60,7 @@ def test_solve_sudoku_easy4():
     """
     sudoku_input_filename = "../data/sudoku_easy4_in.csv"
     sudoku_output_filename = "../data/sudoku_easy4_out.csv"
-    sudoku_input = np.loadtxt(sudoku_input_filename,delimiter=",",dtype="i4")
-    sudoku_output = solve_sudoku(sudoku_input)
-    sudoku_expected_output = np.loadtxt(sudoku_output_filename,delimiter=",",dtype="i4")
-    assert (sudoku_expected_output == sudoku_output).all()
+    verify_sudoku_solution(sudoku_input_filename, sudoku_output_filename)
 
 def test_solve_sudoku_easy5():
     """ Test if the sudoku solver gives the same solution
@@ -79,10 +70,7 @@ def test_solve_sudoku_easy5():
     """
     sudoku_input_filename = "../data/sudoku_easy5_in.csv"
     sudoku_output_filename = "../data/sudoku_easy5_out.csv"
-    sudoku_input = np.loadtxt(sudoku_input_filename,delimiter=",",dtype="i4")
-    sudoku_output = solve_sudoku(sudoku_input)
-    sudoku_expected_output = np.loadtxt(sudoku_output_filename,delimiter=",",dtype="i4")
-    assert (sudoku_expected_output == sudoku_output).all()
+    verify_sudoku_solution(sudoku_input_filename, sudoku_output_filename)
 
 def test_solve_sudoku_easy6():
     """ Test if the sudoku solver gives the same solution
@@ -92,10 +80,7 @@ def test_solve_sudoku_easy6():
     """
     sudoku_input_filename = "../data/sudoku_easy6_in.csv"
     sudoku_output_filename = "../data/sudoku_easy6_out.csv"
-    sudoku_input = np.loadtxt(sudoku_input_filename,delimiter=",",dtype="i4")
-    sudoku_output = solve_sudoku(sudoku_input)
-    sudoku_expected_output = np.loadtxt(sudoku_output_filename,delimiter=",",dtype="i4")
-    assert (sudoku_expected_output == sudoku_output).all()
+    verify_sudoku_solution(sudoku_input_filename, sudoku_output_filename)
 
 def test_solve_sudoku_easy7():
     """ Test if the sudoku solver gives the same solution
@@ -105,10 +90,7 @@ def test_solve_sudoku_easy7():
     """
     sudoku_input_filename = "../data/sudoku_easy7_in.csv"
     sudoku_output_filename = "../data/sudoku_easy7_out.csv"
-    sudoku_input = np.loadtxt(sudoku_input_filename,delimiter=",",dtype="i4")
-    sudoku_output = solve_sudoku(sudoku_input)
-    sudoku_expected_output = np.loadtxt(sudoku_output_filename,delimiter=",",dtype="i4")
-    assert (sudoku_expected_output == sudoku_output).all()
+    verify_sudoku_solution(sudoku_input_filename, sudoku_output_filename)
 
 def test_solve_sudoku_easy8():
     """ Test if the sudoku solver gives the same solution
@@ -118,10 +100,7 @@ def test_solve_sudoku_easy8():
     """
     sudoku_input_filename = "../data/sudoku_easy8_in.csv"
     sudoku_output_filename = "../data/sudoku_easy8_out.csv"
-    sudoku_input = np.loadtxt(sudoku_input_filename,delimiter=",",dtype="i4")
-    sudoku_output = solve_sudoku(sudoku_input)
-    sudoku_expected_output = np.loadtxt(sudoku_output_filename,delimiter=",",dtype="i4")
-    assert (sudoku_expected_output == sudoku_output).all()
+    verify_sudoku_solution(sudoku_input_filename, sudoku_output_filename)
 
 def test_solve_sudoku_easy9():
     """ Test if the sudoku solver gives the same solution
@@ -131,10 +110,7 @@ def test_solve_sudoku_easy9():
     """
     sudoku_input_filename = "../data/sudoku_easy9_in.csv"
     sudoku_output_filename = "../data/sudoku_easy9_out.csv"
-    sudoku_input = np.loadtxt(sudoku_input_filename,delimiter=",",dtype="i4")
-    sudoku_output = solve_sudoku(sudoku_input)
-    sudoku_expected_output = np.loadtxt(sudoku_output_filename,delimiter=",",dtype="i4")
-    assert (sudoku_expected_output == sudoku_output).all()
+    verify_sudoku_solution(sudoku_input_filename, sudoku_output_filename)
 
 def test_solve_sudoku_medium10():
     """ Test if the sudoku solver gives the same solution
@@ -144,10 +120,7 @@ def test_solve_sudoku_medium10():
     """
     sudoku_input_filename = "../data/sudoku_medium10_in.csv"
     sudoku_output_filename = "../data/sudoku_medium10_out.csv"
-    sudoku_input = np.loadtxt(sudoku_input_filename,delimiter=",",dtype="i4")
-    sudoku_output = solve_sudoku(sudoku_input)
-    sudoku_expected_output = np.loadtxt(sudoku_output_filename,delimiter=",",dtype="i4")
-    assert (sudoku_expected_output == sudoku_output).all()
+    verify_sudoku_solution(sudoku_input_filename, sudoku_output_filename)
     
 def test_solve_sudoku_medium11():
     """ Test if the sudoku solver gives the same solution
@@ -157,10 +130,7 @@ def test_solve_sudoku_medium11():
     """
     sudoku_input_filename = "../data/sudoku_medium11_in.csv"
     sudoku_output_filename = "../data/sudoku_medium11_out.csv"
-    sudoku_input = np.loadtxt(sudoku_input_filename,delimiter=",",dtype="i4")
-    sudoku_output = solve_sudoku(sudoku_input)
-    sudoku_expected_output = np.loadtxt(sudoku_output_filename,delimiter=",",dtype="i4")
-    assert (sudoku_expected_output == sudoku_output).all()
+    verify_sudoku_solution(sudoku_input_filename, sudoku_output_filename)
     
 def test_solve_sudoku_medium12():
     """ Test if the sudoku solver gives the same solution
@@ -170,10 +140,7 @@ def test_solve_sudoku_medium12():
     """
     sudoku_input_filename = "../data/sudoku_medium12_in.csv"
     sudoku_output_filename = "../data/sudoku_medium12_out.csv"
-    sudoku_input = np.loadtxt(sudoku_input_filename,delimiter=",",dtype="i4")
-    sudoku_output = solve_sudoku(sudoku_input)
-    sudoku_expected_output = np.loadtxt(sudoku_output_filename,delimiter=",",dtype="i4")
-    assert (sudoku_expected_output == sudoku_output).all()
+    verify_sudoku_solution(sudoku_input_filename, sudoku_output_filename)
     
 def test_solve_sudoku_medium13():
     """ Test if the sudoku solver gives the same solution
@@ -183,10 +150,7 @@ def test_solve_sudoku_medium13():
     """
     sudoku_input_filename = "../data/sudoku_medium13_in.csv"
     sudoku_output_filename = "../data/sudoku_medium13_out.csv"
-    sudoku_input = np.loadtxt(sudoku_input_filename,delimiter=",",dtype="i4")
-    sudoku_output = solve_sudoku(sudoku_input)
-    sudoku_expected_output = np.loadtxt(sudoku_output_filename,delimiter=",",dtype="i4")
-    assert (sudoku_expected_output == sudoku_output).all()
+    verify_sudoku_solution(sudoku_input_filename, sudoku_output_filename)
     
 def test_solve_sudoku_medium14():
     """ Test if the sudoku solver gives the same solution
@@ -196,10 +160,7 @@ def test_solve_sudoku_medium14():
     """
     sudoku_input_filename = "../data/sudoku_medium14_in.csv"
     sudoku_output_filename = "../data/sudoku_medium14_out.csv"
-    sudoku_input = np.loadtxt(sudoku_input_filename,delimiter=",",dtype="i4")
-    sudoku_output = solve_sudoku(sudoku_input)
-    sudoku_expected_output = np.loadtxt(sudoku_output_filename,delimiter=",",dtype="i4")
-    assert (sudoku_expected_output == sudoku_output).all()
+    verify_sudoku_solution(sudoku_input_filename, sudoku_output_filename)
     
 def test_solve_sudoku_medium15():
     """ Test if the sudoku solver gives the same solution
@@ -209,10 +170,7 @@ def test_solve_sudoku_medium15():
     """
     sudoku_input_filename = "../data/sudoku_medium15_in.csv"
     sudoku_output_filename = "../data/sudoku_medium15_out.csv"
-    sudoku_input = np.loadtxt(sudoku_input_filename,delimiter=",",dtype="i4")
-    sudoku_output = solve_sudoku(sudoku_input)
-    sudoku_expected_output = np.loadtxt(sudoku_output_filename,delimiter=",",dtype="i4")
-    assert (sudoku_expected_output == sudoku_output).all()
+    verify_sudoku_solution(sudoku_input_filename, sudoku_output_filename)
     
 def test_solve_sudoku_medium16():
     """ Test if the sudoku solver gives the same solution
@@ -222,10 +180,7 @@ def test_solve_sudoku_medium16():
     """
     sudoku_input_filename = "../data/sudoku_medium16_in.csv"
     sudoku_output_filename = "../data/sudoku_medium16_out.csv"
-    sudoku_input = np.loadtxt(sudoku_input_filename,delimiter=",",dtype="i4")
-    sudoku_output = solve_sudoku(sudoku_input)
-    sudoku_expected_output = np.loadtxt(sudoku_output_filename,delimiter=",",dtype="i4")
-    assert (sudoku_expected_output == sudoku_output).all()
+    verify_sudoku_solution(sudoku_input_filename, sudoku_output_filename)
     
 def test_solve_sudoku_medium17():
     """ Test if the sudoku solver gives the same solution
@@ -235,10 +190,7 @@ def test_solve_sudoku_medium17():
     """
     sudoku_input_filename = "../data/sudoku_medium17_in.csv"
     sudoku_output_filename = "../data/sudoku_medium17_out.csv"
-    sudoku_input = np.loadtxt(sudoku_input_filename,delimiter=",",dtype="i4")
-    sudoku_output = solve_sudoku(sudoku_input)
-    sudoku_expected_output = np.loadtxt(sudoku_output_filename,delimiter=",",dtype="i4")
-    assert (sudoku_expected_output == sudoku_output).all()
+    verify_sudoku_solution(sudoku_input_filename, sudoku_output_filename)
     
 def test_solve_sudoku_hard18():
     """ Test if the sudoku solver gives the same solution
@@ -248,10 +200,7 @@ def test_solve_sudoku_hard18():
     """
     sudoku_input_filename = "../data/sudoku_hard18_in.csv"
     sudoku_output_filename = "../data/sudoku_hard18_out.csv"
-    sudoku_input = np.loadtxt(sudoku_input_filename,delimiter=",",dtype="i4")
-    sudoku_output = solve_sudoku(sudoku_input)
-    sudoku_expected_output = np.loadtxt(sudoku_output_filename,delimiter=",",dtype="i4")
-    assert (sudoku_expected_output == sudoku_output).all()
+    verify_sudoku_solution(sudoku_input_filename, sudoku_output_filename)
     
 def test_solve_sudoku_hard19():
     """ Test if the sudoku solver gives the same solution
@@ -261,10 +210,7 @@ def test_solve_sudoku_hard19():
     """
     sudoku_input_filename = "../data/sudoku_hard19_in.csv"
     sudoku_output_filename = "../data/sudoku_hard19_out.csv"
-    sudoku_input = np.loadtxt(sudoku_input_filename,delimiter=",",dtype="i4")
-    sudoku_output = solve_sudoku(sudoku_input)
-    sudoku_expected_output = np.loadtxt(sudoku_output_filename,delimiter=",",dtype="i4")
-    assert (sudoku_expected_output == sudoku_output).all()
+    verify_sudoku_solution(sudoku_input_filename, sudoku_output_filename)
     
 def test_solve_sudoku_hard20():
     """ Test if the sudoku solver gives the same solution
@@ -274,8 +220,5 @@ def test_solve_sudoku_hard20():
     """
     sudoku_input_filename = "../data/sudoku_hard20_in.csv"
     sudoku_output_filename = "../data/sudoku_hard20_out.csv"
-    sudoku_input = np.loadtxt(sudoku_input_filename,delimiter=",",dtype="i4")
-    sudoku_output = solve_sudoku(sudoku_input)
-    sudoku_expected_output = np.loadtxt(sudoku_output_filename,delimiter=",",dtype="i4")
-    assert (sudoku_expected_output == sudoku_output).all()
+    verify_sudoku_solution(sudoku_input_filename, sudoku_output_filename)
     
